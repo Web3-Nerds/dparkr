e8907fc1698f6c672a27dd8aae371cd9f860883d
shri <shricse07@gmail.com>
2025-06-06 18:26:03 +0530

    fix: body overflow in mobile view

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <body className='h-screen w-full'>
-        <GlobalGradient/>
+        {/* <GlobalGradient/> */}
         <AppProviders>
           {children}
         </AppProviders> 
