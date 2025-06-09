b461766377e96395e2bc40756935e0d0df1d2c1b
shri <shricse07@gmail.com>
2025-06-09 18:47:09 +0530

    add: wallet connection, minor ui changes

--- src/components/account/account-detail-feature.tsx
+++ src/components/account/account-detail-feature.tsx
@@ -27,19 +27,9 @@ export default function AccountDetailFeature() {
 
   return (
     <div>
-      {/* <AppHero */}
-      {/*   title={<AccountBalance address={address} />} */}
-      {/*   subtitle={ */}
-      {/*     <div className="my-4"> */}
-      {/*       <ExplorerLink path={`account/${address}`} label={ellipsify(address.toString())} /> */}
-      {/*     </div> */}
-      {/*   } */}
-      {/* > */}
-      {/*   <div className="my-4"> */}
-      {/*     <AccountButtons address={address} /> */}
-      {/*   </div> */}
-      {/* </AppHero> */}
       <div className="space-y-8">
+        <AccountBalance address={address} />
+        <AccountButtons address={address}/>
         <AccountTokens address={address} />
         <AccountTransactions address={address} />
       </div>
